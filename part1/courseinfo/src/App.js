9a644ff6eb863a61139e20ff384b7cb01c8b0a7e
Lander Joshua Vitug <lander.vitug@gmail.com>
2021-04-30 17:32:21 -0500

    completed part 1 exercise 2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content p1={part1} p2={part2} p3={part3} e1={exercises1} e2={exercises2} e3={exercises3} />
-      <Total te={exercises1 + exercises2 + exercises3} />
+      <Content p1={part1.name} p2={part2.name} p3={part3.name} e1={part1.exercises} e2={part2.exercises} e3={part3.exercises} />
+      <Total te={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
@@ -29,10 +35,17 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-    <p>{props.p1} {props.e1}</p>
-    <p>{props.p2} {props.e2}</p>
-    <p>{props.p3} {props.e3}</p>
+    <Part p={props.p1} e={props.e1}/>
+    <Part p={props.p2} e={props.e2}/>
+    <Part p={props.p3} e={props.e3}/>
     </>
+  )
+}
+
+// p = part; e = exercise;
+const Part = (props) => {
+  return (
+    <p>{props.p} {props.e}</p>
   )
 }
 
