d7e91ed680a5fbc745e636145c749c2cf688a286
Lander Joshua Vitug <lander.vitug@gmail.com>
2021-05-02 16:37:19 -0500

    initialized the phonebook webapp and completed part 2 exercise 6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  // event handler for the form
+  const submitName = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName
+    }
+    setPersons(persons.concat(person))
+  }
+  // event handler for the input
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={submitName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+    </div>
+  )
+}
+
+export default App
