887322ac0812fa4a5ae5ba4db132666effc0602e
Lander Joshua Vitug <lander.vitug@gmail.com>
2021-05-03 18:53:51 -0500

    completed part 2 exercise 11 and initialized the countries webapp for exercise 12+

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
   // state for storing data passed by user input (these ones are dummy data for testing purposes)
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   // state for storing changes in the name input field
   const [ newName, setNewName ] = useState('')
   // state for storing changes in the phone number input field
@@ -18,7 +15,17 @@ const App = () => {
   // state for storing changes in the search field
   const [ newST, setNewST ] = useState('')    // ST = Search Term
   // state for storing tailored data passed by user input (based on the search term)
-  const [ matchedPeople, setMatchedPeople ] = useState(persons)
+  const [ matchedPeople, setMatchedPeople ] = useState([])
+
+  // effect hook 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setMatchedPeople(response.data)
+      })
+  }, [])
 
   // event handler for the form
   const submitInfo = (event) => {
